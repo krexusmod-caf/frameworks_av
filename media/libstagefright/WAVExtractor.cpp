CyanogenMod/android_frameworks_av

libstagefright: fix size_t printf format
size_t should use the portable %zu

Change-Id: I974d4be7d4921a8c3fc684d24721a231c5b91170

media/libstagefright/WAVExtractor.cpp
@@ -437,7 +437,7 @@ status_t WAVSource::read(
         maxBytesToRead = 3*(kMaxFrameSize/4);
     } else
         maxBytesToRead = kMaxFrameSize;
-    ALOGV("%s mBitsPerSample %d, kMaxFrameSize %d, ",
+    ALOGV("%s mBitsPerSample %d, kMaxFrameSize %zu, ",
           __func__, mBitsPerSample, kMaxFrameSize);
 
     size_t maxBytesAvailable =
@@ -504,7 +504,7 @@ status_t WAVSource::read(
             tmp->set_range(0, 4 * numBytes);
             int8_t *dst = (int8_t *)tmp->data();
             const uint8_t *src = (const uint8_t *)buffer->data();
-            ALOGV("numBytes = %d", numBytes);
+            ALOGV("numBytes = %zu", numBytes);
             while(numBytes-- > 0) {
                *dst++ = 0x0;
                *dst++ = src[0];
@@ -514,7 +514,7 @@ status_t WAVSource::read(
             }
             buffer->release();
             buffer = tmp;
-            ALOGV("length = %d", buffer->range_length());
+            ALOGV("length = %zu", buffer->range_length());
         }
     }
 

krexusmod-caf/frameworks_av

media/libstagefright/WAVExtractor.cpp
@@ -437,7 +437,7 @@ status_t WAVSource::read(
         maxBytesToRead = 3*(kMaxFrameSize/4);
     } else
         maxBytesToRead = kMaxFrameSize;
-    ALOGV("%s mBitsPerSample %d, kMaxFrameSize %d, ",
+    ALOGV("%s mBitsPerSample %d, kMaxFrameSize %zu, ",
           __func__, mBitsPerSample, kMaxFrameSize);
 
     size_t maxBytesAvailable =
@@ -504,7 +504,7 @@ status_t WAVSource::read(
             tmp->set_range(0, 4 * numBytes);
             int8_t *dst = (int8_t *)tmp->data();
             const uint8_t *src = (const uint8_t *)buffer->data();
-            ALOGV("numBytes = %d", numBytes);
+            ALOGV("numBytes = %zd", numBytes);
             while(numBytes-- > 0) {
                *dst++ = 0x0;
                *dst++ = src[0];
@@ -514,7 +514,7 @@ status_t WAVSource::read(
             }
             buffer->release();
             buffer = tmp;
-            ALOGV("length = %d", buffer->range_length());
+            ALOGV("length = %zu", buffer->range_length());
         }
     }
 